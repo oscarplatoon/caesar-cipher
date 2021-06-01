55c99adeecb77b8acdadea9942c9a4021f6e8ace
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-06-01 21:22:11 +0000

    Sarah and Tim caesar cypher

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,27 @@
-def caesar_cipher(string, shift_amount):
+def caesar_cipher(string, shift_amount=0):
+   result = ""
+   alphabet = "abcdefghijklmnopqrstuvwxyz"
+   ALPHABET = alphabet.upper()
+   for letter in string:
+       if(letter.islower()):
+           #Move "letter" "shift_amount" times
+           pos = alphabet.find(letter)
+           pos += shift_amount
+           while pos < 0:
+               pos += 26
+           while pos > 25:
+               pos -= 26
+           letter = alphabet[pos]
+       elif(letter.isupper()):
+           #Move "letter" "shift_amount" times
+           pos = ALPHABET.find(letter)
+           pos += shift_amount
+           while pos < 0:
+               pos += 26
+           while pos > 25:
+               pos -= 26
+           letter = ALPHABET[pos]
+       result += letter
+
+   return result
+       
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,7 +1,20 @@
-# Rewrite this in Unit Test
-
+import unittest
 from caesar_cipher import caesar_cipher
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+class CaesarTestCase(unittest.TestCase):
+    """Tests for caesar_cipher.py."""
+    def test_string1(self):
+        self.assertEqual(caesar_cipher("Boy! What a string!", -5) , "Wjt! Rcvo v nomdib!")
+
+    def test_string2(self):
+        self.assertEqual(caesar_cipher("Hello zach168! Yes here.", 5) , "Mjqqt efhm168! Djx mjwj.")
+
+    def test_string2_reverse(self):
+        self.assertEqual(caesar_cipher("Hello zach168! Yes here.", -5) , "Czggj uvxc168! Tzn czmz.")
+
+    def test_string3(self):
+        self.assertEqual(caesar_cipher("BbBbB123!", -5) , "WwWwW123!")
+
+
+if __name__ == '__main__':
+    unittest.main()
