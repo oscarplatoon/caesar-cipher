e1cad59ee807bdcc203207a2286ff78daddc0c78
jakewhite5 <jakewhite5@yahoo.com>
2021-06-01 14:24:26 -0700

    Python solution with Tianlin Cai

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,16 @@
 def caesar_cipher(string, shift_amount):
+    return_cipher = ''
+    for letter in string:
+        # maintain space characters 
+        if not letter.isalpha():
+            return_cipher += letter
+        # encrypts uppercase characters without removing case
+        elif letter.isupper():
+            #convert letter to unicode, subtract base unicode to get place in alpha
+            #A=65, A=95 A=30 A=4 A=69
+            return_cipher += chr((ord(letter) + shift_amount - 65) % 26 + 65)
+        elif letter.islower():
+            # same but with lowercase
+            return_cipher += chr((ord(letter) + shift_amount - 97) % 26 + 97)
+    return(return_cipher)    
+# caesar_cipher("Boy! What a string!", -5)
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,7 +1,23 @@
 # Rewrite this in Unit Test
 
 from caesar_cipher import caesar_cipher
+import unittest
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+class CaesarCipher(unittest.TestCase):
+
+    def test_for_functionality1(self):
+        self.assertEqual(caesar_cipher("Boy! What a string!", -5), "Wjt! Rcvo v nomdib!")
+
+    def test_for_functionality2(self):
+        self.assertEqual(caesar_cipher("Hello zach168! Yes here.", 5), "Mjqqt efhm168! Djx mjwj.")
+
+    def test_for_functionality3(self):
+        self.assertEqual(caesar_cipher("Hello Zach168! Yes here.", -5), "Czggj Uvxc168! Tzn czmz.")
+    
+if __name__ == '__main__':
+    unittest.main()
+
+
+# print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
+# print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
+# print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
