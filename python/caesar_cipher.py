d59178cd2ed27c74b999b7f1369c4d86e3733240
ver2point0 <jmci10@hotmail.com>
2021-06-01 15:00:51 -0700

    Andrew and John Caesar Cipher solution

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,27 @@
 def caesar_cipher(string, shift_amount):
+
+
+	list_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	list_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+              'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+	answer = ""
+
+	key = list_lower + list_upper
+
+	for x in range(shift_amount % 26):
+		hold = list_lower.pop(0)
+		list_lower.append(hold)
+		hold = list_upper.pop(0)
+		list_upper.append(hold)
+
+	key_out = list_lower + list_upper
+
+	for y in string:
+		if y.isalpha():
+			answer += key_out[key.index(y)]
+		else:
+			answer += y
+
+	return answer
