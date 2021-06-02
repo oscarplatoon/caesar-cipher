21c433a4c28b51f143dc9c483187c3ca756cc2b5
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-01 22:08:19 -0500

    Jonas' caesar_cipher.py

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,22 @@
-def caesar_cipher(string, shift_amount):
+def caesar_cipher(string, num):
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  new_word = ""
+
+  for i in string:
+    if i in alphabet:
+      if alphabet.index(i) + num >= 0:
+        new_word += alphabet[(alphabet.index(i) + num) % 26]
+      else:
+        new_word += alphabet[26 + (alphabet.index(i) + num)]
+
+    elif i in caps:
+      if caps.index(i) + num >= 0:
+        new_word += caps[(caps.index(i) + num) % 26]
+      else:
+        new_word += caps[26 + (caps.index(i) + num)] 
+
+    else:
+      new_word += i
+
+  return new_word
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,7 +1,26 @@
-# Rewrite this in Unit Test
-
 from caesar_cipher import caesar_cipher
+import unittest
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+class TestCaesarCipher(unittest.Testcase):
+    ''' tests for caesar_cipher.py'''
+    
+    ''' when you call caesar_cipher, you get a string back.'''
+    def test_returns_a_string(self):
+        self.assertTrue(type(caesar_cipher("test", 0) == str))
+    
+    ''' when you call caesar_cipher, output accuracy is correct.'''
+    def test_output_accuracy(self):
+        self.assertTrue(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
+        self.assertTrue(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
+        
+    ''' Challenge questions'''
+    def test_function_challenge(self):
+        self.assertTrue(caesar_cipher("Hello Zach168! Yes here.", -39) == "Uryyb Mnpu168! Lrf urer.")
+        self.assertTrue(caesar_cipher("What. Is. This... ?!", 2) == "Yjcv. Ku. Vjku... ?!")
+        self.assertTrue(caesar_cipher("Gut check, folks!", 24) == "Esr afcai, dmjiq!")
+        
+if __name__ == '__main__':
+    unittest.main()
+        
+
+
